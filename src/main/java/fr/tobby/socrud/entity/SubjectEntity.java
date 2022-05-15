6938c239c1f9f544abca153344a801359646c3d4
GabrielBouhnik <gabriel.bouhnik@epita.fr>
2022-05-15 17:45:21 +0200

    add notnull annotation in subjectmodel and create subjectentity class

---
+++ src/main/java/fr/tobby/socrud/entity/SubjectEntity.java
@@ -0,0 +1,28 @@
+package fr.tobby.socrud.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subjects")
+@NoArgsConstructor
+@Getter
+public class SubjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String description;
+
+    public SubjectEntity(@NotNull String title,
+            @NotNull String description) {
+        this.id = null;
+        this.title = title;
+        this.description = description;
+    }
+}
--- src/main/java/fr/tobby/socrud/model/SubjectModel.java
+++ src/main/java/fr/tobby/socrud/model/SubjectModel.java
@@ -1,16 +1,21 @@
-package fr.tobby.socrud.models;
+package fr.tobby.socrud.model;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
 
 @Getter
 @JsonAutoDetect
 public final class SubjectModel {
     private final long id;
+    @NotNull
     private final String title;
+    @NotNull
     private final String description;
 
-    public SubjectModel(long id, String title, String description) {
+    public SubjectModel(long id,
+                        @NotNull String title,
+                        @NotNull String description) {
         this.id = id;
         this.title = title;
         this.description = description;
