a4d87a6dc98a11d91427e912ec3962077189b1e4
GabrielBouhnik <gabriel.bouhnik@epita.fr>
2022-05-31 13:34:22 +0200

    add jwt filter and token manager

--- src/main/java/fr/tobby/socrud/WebSecurityConfig.java
+++ src/main/java/fr/tobby/socrud/WebSecurityConfig.java
@@ -1,20 +1,53 @@
 package fr.tobby.socrud;
 
+import fr.tobby.socrud.auth.JwtFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtFilter jwtFilter;
+
+    public WebSecurityConfig(final JwtFilter jwtFilter)
+    {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception
     {
         http.authorizeHttpRequests()
                 .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/admin/**").authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder encoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean(name = "myAuthenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
     }
 }
---
+++ src/main/java/fr/tobby/socrud/exception/TokenExpiredException.java
@@ -0,0 +1,6 @@
+package fr.tobby.socrud.exception;
+
+public class TokenExpiredException extends RuntimeException {
+    public TokenExpiredException() {
+    }
+}
