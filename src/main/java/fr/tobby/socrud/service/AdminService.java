5b2857b5d1b479b8d951fac5922be7b86a707e82
Yanis Chaabane <yanis.chaabane@epita.fr>
2022-05-31 13:43:19 +0200

    feat(admin): encode the password

--- src/main/java/fr/tobby/socrud/service/AdminService.java
+++ src/main/java/fr/tobby/socrud/service/AdminService.java
@@ -3,19 +3,22 @@ package fr.tobby.socrud.service;
 import fr.tobby.socrud.entity.AdminEntity;
 import fr.tobby.socrud.model.request.CreateAccountRequest;
 import fr.tobby.socrud.repository.AdminRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminService {
     private final AdminRepository adminRepository;
+    private final PasswordEncoder encoder;
 
-    public AdminService(final AdminRepository adminRepository) {
+    public AdminService(final AdminRepository adminRepository, final PasswordEncoder encoder) {
         this.adminRepository = adminRepository;
+        this.encoder = encoder;
     }
 
     @Transactional
     public void create(CreateAccountRequest request) {
-        adminRepository.save(AdminEntity.of(request.getLogin(), request.getPassword()));
+        adminRepository.save(AdminEntity.of(request.getLogin(), encoder.encode(request.getPassword())));
     }
 }
