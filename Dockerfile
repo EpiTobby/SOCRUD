40400c4b9d085a7bcbc120f02f1ced84f5967b03
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-06-07 17:19:17 +0200

    security: default admin user
    Signed-off-by: Gabriel Rayzal <gabriel.rayzal@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM openjdk:17.0-jdk
 
 COPY target/socrud-0.0.1-SNAPSHOT.jar app.jar
 
+ENV spring.security.user.name ${DEFAULT_USERNAME}
+ENV spring.security.user.password ${DEFAULT_PASSWORD}
+
 ENTRYPOINT java -Dserver.port=8080 -jar app.jar
--- src/main/java/fr/tobby/socrud/WebSecurityConfig.java
+++ src/main/java/fr/tobby/socrud/WebSecurityConfig.java
@@ -2,6 +2,14 @@ package fr.tobby.socrud;
 
 import fr.tobby.socrud.auth.AuthenticationService;
 import fr.tobby.socrud.auth.JwtFilter;
+import fr.tobby.socrud.exception.AdminCreationFailed;
+import fr.tobby.socrud.model.request.CreateAccountRequest;
+import fr.tobby.socrud.service.AdminService;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +27,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
     private final AuthenticationService service;
 
     private final JwtFilter jwtFilter;
@@ -42,6 +51,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected UserDetailsService userDetailsService() {
         return service;
+    }
+
+    @Bean
+    public CommandLineRunner createDefaultUser(@Value("${spring.security.user.name}") @Nullable String name,
+                                               @Value("${spring.security.user.password}") @Nullable String password,
+                                               AdminService adminService)
+    {
+        return args -> {
+            if (name == null || password == null || name.isEmpty() || password.isEmpty())
+                return;
+            try
+            {
+                adminService.create(CreateAccountRequest.builder().login(name).password(password).build());
+                logger.info("Creating default user {}", name);
+            }
+            catch (AdminCreationFailed ignored)
+            {
+                // Account already exists
+            }
+        };
     }
 
     @Bean
